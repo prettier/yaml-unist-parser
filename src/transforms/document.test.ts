a40b7c561db5c283f03bfd113342d3c95eb59e7e
Ika <ikatyang@gmail.com>
2018-08-17 21:44:12 +0800

    test: add tests

--- src/transforms/document.test.ts
+++ src/transforms/document.test.ts
@@ -23,6 +23,7 @@ testCases([
   ["---\nhello\n... #documentEndComment\n", getDocument(0)],
   ['&123 123 "123"\n\n... #123\n #\n\n123\n\n\n ', selectors],
   ["...\n\n#\n\n", selectors],
+  ["#123\n#456\n---", getDocumentHead(0)],
 ]);
 
 function getDocument(documentIndex: number) {
