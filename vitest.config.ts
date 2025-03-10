0f1b5072721a0b0627c559aac803a6a3b8c040aa
fisker Cheung <lionkay@gmail.com>
2025-03-10 13:47:06 +0800

    test: switch `jest` to `vitest` (#293)

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    include: ["**/tests/**/*.ts", "**/*.test.ts"],
+    snapshotSerializers: ["jest-snapshot-serializer-raw"],
+    coverage: {
+      enabled: !!process.env.CI,
+      reporter: ["lcov", "text"],
+      include: ["src/**/*.ts"],
+      exclude: ["src/index.ts", "src/helpers.ts", "src/types.ts"],
+      // FIXME
+      thresholds: {
+        branches: 95,
+        functions: 95,
+        lines: 95,
+        statements: 95,
+      },
+    },
+  },
+});
