54571cc36ceb8da17a3cc5573ebd92d04625198e
fisker <lionkay@gmail.com>
2025-03-10 12:54:00 +0800

    chore: update devDependencies

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ export default {
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "js", "json"],
   testMatch: ["**/tests/**/*.ts", "**/*.test.ts"],
-  extensionsToTreatAsEsm: ['.ts'],
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    '\\.ts$': [
-      'ts-jest',
+    "\\.ts$": [
+      "ts-jest",
       {
         diagnostics: true,
         useESM: true,
@@ -13,23 +13,23 @@ export default {
     ],
   },
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
-  snapshotSerializers: ['jest-snapshot-serializer-raw'],
+  snapshotSerializers: ["jest-snapshot-serializer-raw"],
   coverageReporters: ["lcov", "text-summary"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts"],
   coveragePathIgnorePatterns: [
     "src/index.ts",
     "src/helpers.ts",
-    "src/types.ts"
+    "src/types.ts",
   ],
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
-  }
+      statements: 100,
+    },
+  },
 };
