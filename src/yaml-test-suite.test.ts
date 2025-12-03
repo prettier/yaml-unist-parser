7b29a97bc3c1b98688a0b87709855e430735bb31
fisker <lionkay@gmail.com>
2025-12-04 01:57:02 +0800

    Enable more tests from yaml-test-suite

--- src/yaml-test-suite.test.ts
+++ src/yaml-test-suite.test.ts
@@ -2,28 +2,10 @@ import yamlTestSuite from "yaml-test-suite";
 import { parse } from "./parse.ts";
 
 const bugs = new Set([
-  "2JQS.yaml",
-  "2G84.yaml",
-  "2G84-2.yaml",
-  "DK95.yaml",
-  "G5U8.yaml",
-  "H7TQ.yaml",
-  "MUS6.yaml",
-  "MUS6-2.yaml",
-  "U99R.yaml",
-  "Y79Y.yaml",
-  "Y79Y-3.yaml",
-  "Y79Y-5.yaml",
-  "Y79Y-6.yaml",
-  "Y79Y-7.yaml",
-  "Y79Y-8.yaml",
-  "Y79Y-9.yaml",
-  "Y79Y-10.yaml",
-  "YJV2.yaml",
-
   // The test suite expects a parsing error,
   // but the yaml package does not give a parsing error.
   // https://github.com/eemeli/yaml/blob/086fa6b5bae325da18734750cddee231ce578930/tests/yaml-test-suite.ts#L19
+  "2JQS.yaml",
   "9MMA.yaml",
   "SF5V.yaml",
 ]);
