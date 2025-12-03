c2eef11612275700dbfc796c5c910e8ee09c1f3f
fisker <lionkay@gmail.com>
2025-12-03 18:00:49 +0800

    Add reference

--- src/yaml-test-suite.test.ts
+++ src/yaml-test-suite.test.ts
@@ -23,6 +23,7 @@ const bugs = new Set([
 
   // The test suite expects a parsing error,
   // but the yaml package does not give a parsing error.
+  // https://github.com/eemeli/yaml/blob/086fa6b5bae325da18734750cddee231ce578930/tests/yaml-test-suite.ts#L19
   "9MMA.yaml",
   "SF5V.yaml",
 ]);
