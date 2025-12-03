00d9f8bd4ea205381bb55424261d7c018746fa9f
fisker <lionkay@gmail.com>
2025-12-03 19:03:36 +0800

    Fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vitest/config";
 
-console.log({
-  enabled: !!process.env.CI && !/^18\./.test(process.version),
-});
-
 export default defineConfig({
   test: {
     globals: true,
     include: ["**/tests/**/*.ts", "**/*.test.ts"],
     snapshotSerializers: ["jest-snapshot-serializer-raw"],
     coverage: {
-      enabled: !!process.env.CI && !/^18\./.test(process.version),
+      enabled: !!process.env.CI && !/^v18\./.test(process.version),
       reporter: ["lcov", "text"],
       include: ["src/**/*.ts"],
       exclude: ["src/index.ts", "src/helpers.ts", "src/types.ts"],
