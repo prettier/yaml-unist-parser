4309b035761baf1d040a614a90a122204be9364a
fisker <lionkay@gmail.com>
2025-12-03 11:38:07 +0800

    Test follow mapping

--- src/options.test.ts
+++ src/options.test.ts
@@ -1,22 +1,33 @@
 import { parse } from "./parse.js";
 
-test("duplicate `<<` keys should always be allowed", () => {
-  const text =
-    "x:\n  - &a\n    key1: value1\n  - &b\n    key2: value2\nfoo:\n  bar: baz\n  <<: *a\n  <<: *b";
-  expect(parse(text)).toMatchSnapshot();
-  expect(parse(text, { allowDuplicateKeysInMap: false })).toMatchSnapshot();
-  expect(parse(text, { allowDuplicateKeysInMap: true })).toMatchSnapshot();
-});
+for (const { type, text } of [
+  { type: "mapping", text: "<<: 1\n<<: 2" },
+  { type: "flowMapping", text: '{"<<": 1,"<<": 2}' },
+]) {
+  test(`(${type}): duplicate '<<' keys should always be allowed`, () => {
+    expect(parse(text)).toBeDefined();
+    expect(parse(text, { allowDuplicateKeysInMap: false })).toBeDefined();
+    const ast = parse(text, { allowDuplicateKeysInMap: true });
+    expect(ast).toBeDefined();
+    const node = ast.children[0].children[1].children[0];
+    expect(node?.type).toBe(type);
+  });
+}
 
-test("duplicate keys should throw error when `allowDuplicateKeysInMap` is false (default)", () => {
-  const text = "a: 1\na: 2";
-  expect(() => parse(text)).toThrowErrorMatchingSnapshot();
-  expect(() =>
-    parse(text, { allowDuplicateKeysInMap: false }),
-  ).toThrowErrorMatchingSnapshot();
-});
-
-test("duplicate keys should be allowed when `allowDuplicateKeysInMap` is true", () => {
-  const text = "a: 1\na: 2";
-  expect(parse(text, { allowDuplicateKeysInMap: true })).toMatchSnapshot();
-});
+for (const { type, text } of [
+  { type: "mapping", text: "a: 1\na: 2" },
+  { type: "flowMapping", text: `{"a":1,"a":2}` },
+]) {
+  test(`(${type}): duplicate keys in ${text}`, () => {
+    expect(() => parse(text)).toThrowError(
+      `Map keys must be unique; "a" is repeated`,
+    );
+    expect(() => parse(text, { allowDuplicateKeysInMap: false })).toThrowError(
+      `Map keys must be unique; "a" is repeated`,
+    );
+    const ast = parse(text, { allowDuplicateKeysInMap: true });
+    expect(ast).toBeDefined();
+    const node = ast.children[0].children[1].children[0];
+    expect(node?.type).toBe(type);
+  });
+}
