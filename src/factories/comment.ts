920ac398f310b6ff6ea9962535fdca0bb2c1600b
fisker <lionkay@gmail.com>
2025-12-04 19:04:22 +0800

    createComment

--- src/factories/comment.ts
+++
@@ -1,9 +0,0 @@
-import { type Position } from "../types.ts";
-
-export function createComment(position: Position, value: string) {
-  return {
-    type: "comment",
-    position,
-    value,
-  };
-}
